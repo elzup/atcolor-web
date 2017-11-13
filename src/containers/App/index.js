7d45839f95084ed16bfdfec776daa10c54dd178d
elzup <guild0105@gmail.com>
2017-11-14 08:29:23 +0900

    Metas

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,61 +1,17 @@
 import React from 'react'
-import {
-	BrowserRouter as Router,
-	Route,
-	Link,
-	Redirect,
-} from 'react-router-dom'
-import queryString from 'query-string'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import Home from '../../components/Home'
 
-const SafeHome = props => {
-	const params = queryString.parse(props.location.search)
-	if (params.p) {
-		const page = params.p
-		delete params.p
-		const qs = queryString.stringify(params)
-		const to = page + (qs ? '?' + qs : '')
-		return <Redirect to={props.match.url + to} />
-	}
-	if (true) {
-		return <Home />
-	}
-}
-
-const About = () => (
+const Pixels = () => (
 	<div>
-		<h2>About</h2>
+		<h2>Pixels</h2>
 	</div>
 )
 
-const Topic = ({ match }) => (
+const Questions = ({ match }) => (
 	<div>
-		<h3>{match.params.topicId}</h3>
-	</div>
-)
-
-const Topics = ({ match }) => (
-	<div>
-		<h2>Topics</h2>
-		<ul>
-			<li>
-				<Link to={`${match.url}/rendering`}>Rendering with React</Link>
-			</li>
-			<li>
-				<Link to={`${match.url}/components`}>Components</Link>
-			</li>
-			<li>
-				<Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-			</li>
-		</ul>
-
-		<Route path={`${match.url}/:topicId`} component={Topic} />
-		<Route
-			exact
-			path={match.url}
-			render={() => <h3>Please select a topic.</h3>}
-		/>
+		<h2>Questions</h2>
 	</div>
 )
 
@@ -67,18 +23,18 @@ const App = () => (
 					<Link to="/">Home</Link>
 				</li>
 				<li>
-					<Link to="/about">About</Link>
+					<Link to="/pixels">pixels</Link>
 				</li>
 				<li>
-					<Link to="/topics">Topics</Link>
+					<Link to="/questions">Questions</Link>
 				</li>
 			</ul>
 
 			<hr />
 
-			<Route exact path="/" component={SafeHome} />
-			<Route path="/about" component={About} />
-			<Route path="/topics" component={Topics} />
+			<Route exact path="/" component={Home} />
+			<Route path="/pixels" component={Pixels} />
+			<Route path="/questions" component={Questions} />
 		</div>
 	</Router>
 )
