51f2f729029a818c9543500d0ef3af12ec1123f7
elzup <guild0105@gmail.com>
2017-11-14 11:09:32 +0900

    Add flag

--- src/containers/App/logic.js
+++ src/containers/App/logic.js
@@ -1,11 +1,12 @@
 // @flow
 import type { ThunkAction } from '../../types'
-import { receiveQuestions } from '../QuestionsContainer/actions'
+import { receiveQuestions, flagIsHere } from '../QuestionsContainer/actions'
 import { receiveUsers } from '../UsersContainer/actions'
 import * as client from '../../api/client'
 
 export function loadQuestions(): ThunkAction {
 	return async dispatch => {
+		dispatch(flagIsHere('FLAG_FLOW_REDUX_PERSIST'))
 		const res = await client.getQuestions()
 		dispatch(receiveQuestions(res.questions))
 		dispatch(receiveUsers(res.users))
