156cbe2b279c89e7c268187c84c7c0baf16e0f7f
elzup <guild0105@gmail.com>
2017-11-14 12:38:53 +0900

    Sync for s2s

--- src/containers/QuestionsContainer/reducer.js
+++ src/containers/QuestionsContainer/reducer.js
@@ -8,6 +8,9 @@ export const initialState: State = []
 
 export default function(state: State = initialState, action: Action): State {
 	switch (action.type) {
+		case Actions.FLAG_IS_HERE:
+			return state
+
 		case Actions.RECEIVE_QUESTIONS:
 			return [...state, ...action.questions.map(u => u.id)]
 
